37fe2d23de1e0e31633b049723daceec8235f484
Muhammad Zahid <muhammadali775@gmail.com>
2021-07-06 02:35:07 -0400

    commit tree 2893e521860ed24b1f1604e781d74f189e07652f

--- app/controllers/dog_houses_controller.rb
+++ app/controllers/dog_houses_controller.rb
@@ -6,6 +6,24 @@ class DogHousesController < ApplicationController
     render json: dog_house
   end
 
+
+
+  def reviews_index
+   dog_house= DogHouse.find(params[:dog_house_id])
+   render json: dog_house.reviews 
+  end
+  
+
+  def review
+    dog_house= DogHouse.find(params[:dog_house_id])
+    review= dog_house.reviews.find(params[:review_id])
+     render json: review 
+  end
+
+
+
+
+
   private
 
   def render_not_found_response
